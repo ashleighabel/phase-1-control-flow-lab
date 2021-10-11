593e0fb55e1699c5567f62ea39de020d7295b706
Ashleigh Abel <ashleighburgess1121@gmail.com>
2021-10-11 16:56:43 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+ let result
+ if (someValue <= 400) {
+   result = "This one is on me!"
+ } else if (someValue > 2000 && someValue < 2500) {
+   result = "I will gladly take your thirty bucks."
+ } else if (someValue > 2500) {
+   result = "No can do."
+ }
+ return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(somePlace){
+  
+  return (somePlace === ("NYC") ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+  let reply;
+  switch (someTip) {
+    case 'generous':
+      reply = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      reply = 'Thank you.';
+      break;
+    default:
+      reply = 'Bye.';
+      break;
+  }
+  return reply
 }
